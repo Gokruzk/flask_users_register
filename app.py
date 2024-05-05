5ced4e5d2e07965082525b4057265b958cb80477
Gokruzk <nigell@gmail.com>
2024-05-04 21:13:24 -0500

    feat: upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify, Response
+from flask_pymongo import PyMongo
+from bson import json_util, ObjectId
+
+app = Flask(__name__)
+database = 'orchestra'
+app.config['MONGO_URI'] = f'mongodb://localhost:27017/{database}'
+mongo = PyMongo(app)
+
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    # receiving data
+    username = request.json['username']
+
+    email = request.json['email']
+    # insert into mongo collection
+    us = mongo.db.users.insert_one(
+        {
+            'username': username,
+            'email': email
+        }
+    )
+    response = {
+        'id': str(us),
+        'username': username,
+        'email': email
+    }
+    return response
+
+
+@app.route('/user', methods=['GET'])
+def get_users():
+    # retreiving data
+    us = mongo.db.users.find()
+    response = json_util.dumps(us)
+    return Response(response, mimetype='application/json')
+
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=True)
